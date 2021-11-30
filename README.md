61a785fca85e135c32621266b200117449ae9cca
danangkonang <dngrifai21@gmail.com>
2021-11-30 16:08:09 -0600

    docs installation

--- README.md
+++ README.md
@@ -1,19 +1,51 @@
 # Aplikasi (POS) poin of sale web php codeigniter
 Aplikasi penjualan berbasis web mempermudah pengusaha memantau penjualan harian/bulanan, stok barang, laba/rugi dan laporan-laporan. Dibuat dengan Codeigniter, MySQL, dan Bootstrap
 
-## cara install
+# Cara Install
+
+## Clonning repositori
 
 ```bash
-$ git clone https://github.com/danangkonang/aplication-poin-of-sale-web-php-codeigniter.git pos
+git clone https://github.com/danangkonang/aplication-poin-of-sale-web-php-codeigniter.git pos
 
-$ cd pos
-
-$ php -S localhost:8080
+cd pos
+cp .env.example .env
 ```
 
-open your browser with address http://localhost:8080
+## Composer
+
+kalau sudah memiliki coposer silahkan install vendor seperti biasanya dan skip langkah ini 
+
+```php
+wget "https://raw.githubusercontent.com/composer/getcomposer.org/main/web/installer"
+
+php installer
+
+php composer.phar install
+```
+
+## Database
+Bisa juga menggunakan xampp, lamp, dll. sesuaikan config .env
+```
+docker-compose up --build -d
+```
+
+## Migrasi database
+
+```bash
+vendor/bin/phinx migrate -e development
+
+vendor/bin/phinx seed:run
+```
+
+## Testing
+
+```php
+php -S localhost:8080
+```
+
+Akses http://localhost:8080 di browser
 
 Happy Coding
 
-<!-- ## Contribusi
-* email: dngrifai21@gmail.com -->
+<!-- danangkonang21@gmail.com -->
--- phinx.php
+++ phinx.php
@@ -7,40 +7,40 @@ $dotenv->load();
 
 return
 [
-    'paths' => [
-        'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
-        'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds'
+  'paths' => [
+    'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
+    'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds'
+  ],
+  'environments' => [
+    'default_migration_table' => 'phinxlog',
+    'default_environment' => 'development',
+    'production' => [
+      'adapter' => 'mysql',
+      'host' => $_ENV['BD_HOST'],
+      'name' => $_ENV['DB_NAME'],
+      'user' => $_ENV['DB_USER'],
+      'pass' => $_ENV['DB_PASSWORD'],
+      'port' => '3306',
+      'charset' => 'utf8',
     ],
-    'environments' => [
-        'default_migration_table' => 'phinxlog',
-        'default_environment' => 'development',
-        'production' => [
-            'adapter' => 'mysql',
-            'host' => 'localhost',
-            'name' => 'production_db',
-            'user' => 'root',
-            'pass' => '',
-            'port' => '3306',
-            'charset' => 'utf8',
-        ],
-        'development' => [
-            'adapter' => 'mysql',
-            'host' => $_ENV['BD_HOST'],
-            'name' => $_ENV['DB_NAME'],
-            'user' => $_ENV['DB_USER'],
-            'pass' => $_ENV['DB_PASSWORD'],
-            'port' => '3306',
-            'charset' => 'utf8',
-        ],
-        'testing' => [
-            'adapter' => 'mysql',
-            'host' => 'localhost',
-            'name' => 'testing_db',
-            'user' => 'root',
-            'pass' => '',
-            'port' => '3306',
-            'charset' => 'utf8',
-        ]
+    'development' => [
+      'adapter' => 'mysql',
+      'host' => $_ENV['BD_HOST'],
+      'name' => $_ENV['DB_NAME'],
+      'user' => $_ENV['DB_USER'],
+      'pass' => $_ENV['DB_PASSWORD'],
+      'port' => '3306',
+      'charset' => 'utf8',
     ],
-    'version_order' => 'creation'
+    'testing' => [
+      'adapter' => 'mysql',
+      'host' => $_ENV['BD_HOST'],
+      'name' => $_ENV['DB_NAME'],
+      'user' => $_ENV['DB_USER'],
+      'pass' => $_ENV['DB_PASSWORD'],
+      'port' => '3306',
+      'charset' => 'utf8',
+    ]
+  ],
+  'version_order' => 'creation'
 ];
